1bc708b4c3fce0475699f07330396aa58216c97a
benceSelmeci <107436947+benceSelmeci@users.noreply.github.com>
2022-06-22 13:06:32 +0200

    Create errors.py

---
+++ arithmetic_formatter/errors.py
@@ -0,0 +1,7 @@
+general = "General programming failure."
+operator = "Error: Operator must be '+' or '-'."
+numLength = "Error: Numbers cannot be more than four digits."
+numType = "Error: Numbers must only contain digits."
+numOfProblems = "Error: Too many problems."
+
+noError = None
